ff1f144d8c5e404b4d58697f8728ff3a7475bd69
Trevor Li <trevorli77@hotmail.com>
2015-08-10 16:51:45 +0300

    modal interface window

--- src/main/resources/assets/app/js/intfaces/intfaces-modal-controller.js
+++ src/main/resources/assets/app/js/intfaces/intfaces-modal-controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('service-testing-tool').controller('IntfacesController', ['$scope', 'Intfaces', '$stateParams', '$state', 'uiGridConstants',
-  function($scope, Intfaces, $stateParams, $state, uiGridConstants) {
+angular.module('service-testing-tool').controller('IntfacesController', ['$scope', 'Intfaces', '$location', '$stateParams', '$state', 'uiGridConstants', '$modalInstance', 'context',
+  function($scope, Intfaces, $location, $stateParams, $state, uiGridConstants, $modalInstance, context) {
     $scope.schema = {
       type: "object",
       properties: {
@@ -55,13 +55,46 @@ angular.module('service-testing-tool').controller('IntfacesController', ['$scope
       }
     ];
 
+    $scope.selectedIntfaces = [];
+
     $scope.intface = {};
 
     $scope.alerts = [];
 
+    $scope.isReturn = function() {
+      if ($scope.context) {
+        return true;
+      }
+
+      return false;
+    };
+
+    $scope.isSelect = function() {
+      if ($scope.context) {
+        if ($scope.context.expect) {
+          return true;
+        }
+      }
+
+      return false;
+    };
+
+    $scope.isMultiSelect = function() {
+      if ($scope.context) {
+        if ($scope.context.expect) {
+          if ($scope.context.expect === "Multi") {
+            return true;
+          }
+        }
+      }
+
+      return false;
+    };
+
     $scope.gridOptions = {
       paginationPageSizes: [10,20,50,100], paginationPageSize: 10,
       enableFiltering: true,
+      enableSelectionBatchEvent: false,
       columnDefs: [
         {
           name: 'name', width: 200, minWidth: 100,
@@ -77,7 +110,20 @@ angular.module('service-testing-tool').controller('IntfacesController', ['$scope
         {
           name: 'deftype', displayName: "Definition Type", width: 200, minWidth: 100
         }
-      ]
+      ],
+      onRegisterApi: function(gridApi){
+        //set gridApi on scope
+        $scope.gridApi = gridApi;
+        if (gridApi.selection) {
+          gridApi.selection.on.rowSelectionChanged($scope,function(row){
+            if (row.isSelected) {
+              $scope.selectedIntfaces.push(row.entity);
+            } else {
+              $scope.selectedIntfaces = _.without($scope.selectedIntfaces, row.entity);
+            }
+          });
+        }
+      }
     };
 
     $scope.create_update = function(form) {
@@ -113,10 +159,35 @@ angular.module('service-testing-tool').controller('IntfacesController', ['$scope
     };
 
     $scope.find = function() {
+      $scope.context = context;
+
       Intfaces.query(function(intfaces) {
+        if ($scope.context) {
+          intfaces = _.filter(intfaces, function(intface) {
+            return ! _.contains($scope.context.intfaceIds, intface.id)
+          });
+        }
         $scope.intfaces = intfaces;
         $scope.gridOptions.data = intfaces;
       });
+    };
+
+    $scope.return = function() {
+      $modalInstance.close();
+    };
+
+    $scope.select = function() {
+      if ($scope.intface.id) {
+        $scope.context.model.intfaceId = $scope.intface.id;
+
+        Intfaces.get({
+          intfaceId: $scope.context.model.intfaceId
+        }, function(intface) {
+          $scope.context.model.intface = intface;
+        });
+      } else {
+        $modalInstance.close($scope.selectedIntfaces);
+      }
     };
 
     $scope.findOne = function() {
