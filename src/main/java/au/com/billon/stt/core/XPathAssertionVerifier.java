313d02979c4192429ff96fc0c3198c9cb0e6d3fe
Trevor Li <trevorli77@hotmail.com>
2015-08-21 16:57:25 +0300

    Simplify Java code

--- src/main/java/au/com/billon/stt/core/XPathAssertionVerifier.java
+++ src/main/java/au/com/billon/stt/core/XPathAssertionVerifier.java
@@ -11,10 +11,7 @@ public class XPathAssertionVerifier implements AssertionVerifier {
     public Assertion verify(Assertion assertion) {
         AssertionVerification verification = assertion.getVerification();
         XPathAssertionProperties assertionProperties = (XPathAssertionProperties) assertion.getProperties();
-        EvaluationRequest evaluationRequest = new EvaluationRequest(
-                assertion.getType(), assertionProperties.getxPath(), verification.getInput(),
-                new XPathEvaluationRequestProperties(assertionProperties.getNamespacePrefixes()));
-        EvaluationResponse evaluationResponse = new EvaluatorFactory().createEvaluator(evaluationRequest).evaluate();
+        EvaluationResponse evaluationResponse = new EvaluatorFactory().createEvaluator("WSDL", assertion.getType()).evaluate(verification.getInput(), assertionProperties);
         verification.setPassed(evaluationResponse.getError() == null &&
                 assertionProperties.getExpectedValue().equals(evaluationResponse.getActualValue()));
         verification.setError(evaluationResponse.getError());
